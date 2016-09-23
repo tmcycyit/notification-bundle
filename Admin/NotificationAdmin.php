85569c0251fc7beadaa35b45a945e8d8c3336e20
Suren <shsuserh@gmail.com>
2016-09-23 14:28:31 +0400

    Add notification in admin panel

---
+++ Admin/NotificationAdmin.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tmcycyit\NotificationBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+
+
+class NotificationAdmin extends Admin
+{
+
+    /**
+     * This function used to show entity fields detailed
+     * @param ShowMapper $showMapper
+     */
+
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+                ->add('header')
+                ->add('fromUser')
+                ->add('created');
+    }
+
+    /**
+     * This function used to show entity field with actions
+     *
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $list list
+     */
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+                ->addIdentifier('header')
+                ->addIdentifier('fromUser')
+                ->addIdentifier('created')
+                ->add('_action', 'actions', array ('actions' => array (
+                        'delete' => array ()
+                )));
+    }
+
+    /**
+     * This function used to add new entity
+     *
+     * @param FormMapper $formMapper formMapper
+     */
+    public function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('header')
+            ->add('fromUser')
+            ->add('created');
+    }
+
+    /**
+     * This function is used to filter entities
+     *
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('header')
+            ->add('fromUser')
+            ->add('created','doctrine_orm_datetime_range', array(),
+                'sonata_type_date_range',
+                array(
+                    'required' => false,
+                    'widget' => 'single_text',
+                    'attr' => array(
+                        'class' => 'datepicker',
+                        'data-date-format' => 'YYYY-MM-DD'
+                    )
+                )
+            );
+    }
+
+}
